0fde0726c42240f38f1609b9c14f58633d003632
baokhongnao <hoanglephibao.1212@gmail.com>
2024-07-30 20:45:04 +0700

    update 2 lan 2 trang 2

--- jsShowdata_2.js
+++ jsShowdata_2.js
@@ -127,6 +127,21 @@ async function editItem(id) {
     // Show the edit form modal
     $('#editModal').modal('show');
 }
+  //đếm số record
+  fetch('https://6605116c2ca9478ea17f2d5d.mockapi.io/tenduongdanang/videosanpham')
+  .then(response => response.json())
+  .then(data => {
+    // Đếm số lượng records
+    const recordCount = data.length;
+
+    // Hiển thị trong HTML
+    const countElement = document.getElementById('recordCount');
+    countElement.textContent = `Số lượng records: ${recordCount}`;
+  })
+  .catch(error => {
+    console.error('Lỗi khi lấy dữ liệu từ API:', error);
+  });
+
 
 // Function to handle edit form submission
 document.getElementById('editTrackingForm').addEventListener('submit', async function (event) {
